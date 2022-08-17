8b1c7cd041494be027eb447e020774e17c058eee
Tschibo00 <ballhause@gmx.de>
2022-08-17 11:18:20 +0200

    adjusted bme280 operating mode

--- my_sensor.h
+++ my_sensor.h
@@ -74,6 +74,11 @@ void calculateRainAmount(){
  */
 void setupSensor(){
   bme.begin(0x76);
+  bme.setSampling(Adafruit_BME280::MODE_FORCED,
+                  Adafruit_BME280::SAMPLING_X1, // temperature
+                  Adafruit_BME280::SAMPLING_X1, // pressure
+                  Adafruit_BME280::SAMPLING_X1, // humidity
+                  Adafruit_BME280::FILTER_OFF   );
   attachInterrupt(PIN_RAIN, onRainTrigger, FALLING);
 }
 
